341c4ae23c4f465a0713d8202332e96b0b0b55f3
Tim Kent <timmeekay@users.noreply.github.com>
2022-05-19 15:39:19 -0700

    Delete app directory

--- app/controllers/visitors_controller.rb
+++
@@ -1,9 +0,0 @@
-class VisitorsController < ApplicationController
-
-  def new
-    @owner = Owner.new
-    flash.now[:notice] = 'Welcome'
-    flash.now[:alert] = 'My Birthday is soon'
-  end
-
-end
